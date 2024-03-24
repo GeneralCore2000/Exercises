ffd409d90aac85ff0c4961012968cac9a570fd20
John Lloyd <78johnlloyd@gmail.com>
2024-03-24 16:52:37 +0800

    Fixed logical error

--- SumOfDecimalToNonDecimal/src/SumOfDecimalToNonDecimal/SumOfDecimalToNonDecimal.java
+++ SumOfDecimalToNonDecimal/src/SumOfDecimalToNonDecimal/SumOfDecimalToNonDecimal.java
@@ -7,14 +7,25 @@ public class SumOfDecimalToNonDecimal {
     public SumOfDecimalToNonDecimal() {
         Scanner in = new Scanner(System.in);
         double num1 = 0, num2 = 0;
-        System.out.print("Enter 1st number >>: ");
-        num1 = in.nextDouble();
-        System.out.print("Enter 2nd number >>: ");
-        num2 = in.nextDouble();
-        if (getSum(num1, num2) % 1 == 0) {
-            System.out.println(getSum((int)num1,(int)num2));
-        }else{
-            System.out.println(getSum(num1,num2));
+        int counter = 0;
+        while (counter < 3) {
+            System.out.print("Enter number #1 >>: ");
+            num1 = in.nextDouble();
+            System.out.print("Enter number #2 >>: ");
+            num2 = in.nextDouble();
+            System.out.print("Sum: ");
+
+            if (getSum(num1, num2) % 1 == 0) {
+                if (num1 % 1 != 0 || num2 % 1 != 0) {
+                    System.out.printf("%.0f", getSum(num1, num2));
+                    System.out.println();
+                } else {
+                    System.out.println(getSum((int) num1, (int) num2));
+                }
+            } else {
+                System.out.println(getSum(num1, num2));
+            }
+            counter++;
         }
     }
 
